04f895edd6c4ed6fd554517fc375cb0506ce873e
Raj2902Patel <rajpatel.imscit20@gmail.com>
2024-07-14 19:26:37 +0530

    Registration Page Design!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "./pages/home";
-import { About } from "./pages/about";
-import { Contact } from "./pages/contact";
-import { Service } from "./pages/service.jsx";
-import { Register } from "./pages/register";
-import { Login } from "./pages/login";
+import { Home } from "./pages/Home/";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Service } from "./pages/Service";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
+import { Navbar } from "./components/Navbar";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -1,3 +1,119 @@
+import { useState } from "react";
+
 export const Register = () => {
-  return <h1>Hello Register Page</h1>;
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+
+  const handleInput = (e) => {
+    // console.log(e);
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(user);
+  }
+
+  return (
+    <>
+      <section>
+        <main>
+          <div className="section-register">
+            <div className="container grid grid-two-cols">
+              <div className="registration-image">
+                <img
+                  src="/images/register.png"
+                  alt="a user is trying to do registration"
+                  width="500"
+                  height="500"
+                />
+              </div>
+
+              <div className="registration-from">
+                <h1 className="main-heading mb-3">Registration Form</h1>
+                <br />
+
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    <label htmlFor="username">username</label>
+                    <input
+                      type="text"
+                      name="username"
+                      placeholder="username"
+                      id="username"
+                      required
+                      autoComplete="off"
+                      value={user.username}
+                      onChange={handleInput}
+                    ></input>
+                  </div>
+
+                  <div>
+                    <label htmlFor="email">email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="enter your email"
+                      id="email"
+                      required
+                      autoComplete="off"
+                      value={user.email}
+                      onChange={handleInput}
+
+                    ></input>
+                  </div>
+
+                  <div>
+                    <label htmlFor="phone">phone</label>
+                    <input
+                      type="number"
+                      name="phone"
+                      placeholder="phone"
+                      id="phone"
+                      required
+                      autoComplete="off"
+                      value={user.phone}
+                      onChange={handleInput}
+
+                    ></input>
+                  </div>
+
+                  <div>
+                    <label htmlFor="password">password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="enter your password"
+                      id="password"
+                      required
+                      autoComplete="off"
+                      value={user.password}
+                      onChange={handleInput}
+
+                    ></input>
+                  </div>
+
+                  <br />
+
+                  <button type="submit" className="btn btn-submit">
+                    Register Now
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </>
+  );
 };
